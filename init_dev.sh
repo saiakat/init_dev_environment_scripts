47dd26c79e36de2e462afcbd9594a9e63c1783cb
saiakat <maxstender1@gmail.com>
2025-09-01 10:59:49 +0200

    small change

--- init_dev.sh
+++ init_dev.sh
@@ -5,17 +5,29 @@ sudo apt update
 
 echo "==== installing gcc and dev essentials ===="
 sudo apt install build-essential
+
 echo "==== installing other dev tools ===="
 sudo apt install -y \
     curl \
     wget \
     git \
     htop 
+
 echo "==== installing node ===="
 bash node.sh
+
 echo "==== installing neovim ===="
 bash neovim.sh
+
 echo "==== installing tmux ===="
 sudo apt install tmux
+
+read -p "Do you want to install oh my bash? (y/n)" yn
+case "$yn" in
+    [Yy]* ) break;;
+    [Nn]* ) echo "Exiting..."; exit;;
+    * ) echo "Invalid input. Please answer yes or no."; exit 1;;
+esac
+
 echo "==== installing oh by bash ===="
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
